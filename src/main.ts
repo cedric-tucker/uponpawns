cfdb971096440943a55684d42a1c5939072d79b1
cedric-tucker <cedtucker03@gmail.com>
2026-08-10 18:38:15 -0400

    Move validation via chessops

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,44 @@
-import './style.css'
 import '@lichess-org/chessground/assets/chessground.base.css';
 import '@lichess-org/chessground/assets/chessground.brown.css';
 import '@lichess-org/chessground/assets/chessground.cburnett.css';
 
 import { Chessground } from '@lichess-org/chessground';
+import { Chess } from 'chessops/chess';
+import { chessgroundDests } from 'chessops/compat';
+import { parseSquare } from 'chessops/util';
+
+import './style.css'
 
 const board = document.getElementById('board');
 if (!board) throw new Error('Board not found');
-const ground = Chessground(board, {});
+
+const pos = Chess.default();
+
+const ground = Chessground(board, {
+  movable: {
+    free: false,
+    color: 'white',
+    dests: chessgroundDests(pos)
+  }
+});
+
+ground.set({
+  movable: {
+    events: {
+      after : (orig, dest) => {
+        pos.play({
+          from: parseSquare(orig)!,
+          to: parseSquare(dest)!,
+        });
+
+      ground.set({
+        turnColor: pos.turn,
+        movable: { 
+          color: pos.turn,
+          dests: chessgroundDests(pos),
+      },
+      });
+      },
+    },
+  },
+});
